95d29d0b4f13a8bab3a25e367271e088f80b3e83
Dartis4 <daneartis4@gmail.com>
2022-05-03 11:59:04 -0600

    Add code for parallel frame processing in prove03, some formatting on setup_files_dirs.py

--- week03/assignment/setup_files_dirs.py
+++ week03/assignment/setup_files_dirs.py
@@ -6,9 +6,11 @@ Program to split videos into individual files
 import os
 import platform
 
+
 def create_dir(folder):
     if not os.path.exists(folder):
         os.makedirs(folder)
+
 
 def create_images(video_file, folder):
     # limit the number of frames to 300
@@ -17,6 +19,7 @@ def create_images(video_file, folder):
     else:
         command = rf'./ffmpeg -i {video_file} -vframes 300 {folder}/image%3d.png'
     os.system(command)
+
 
 def main():
     # Create sub folders 
@@ -27,5 +30,6 @@ def main():
     # Create the image files
     create_images('elephants.mp4', 'elephant')
     create_images('green.mp4', 'green')
+
 
 main()
