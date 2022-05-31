1e067f0438d18ecc9997524dba04f569f8b9c5c0
Dartis4 <daneartis4@gmail.com>
2022-05-31 13:13:24 -0600

    finish Team 06

--- week06/team/team.py
+++ week06/team/team.py
@@ -22,7 +22,13 @@ import filecmp
 # Include cse 251 common Python files
 from cse251 import *
 
-def sender():
+
+BLOCK_SIZE = (1024 * 4)
+
+END_MESSAGE = 'ALL DONE'
+
+
+def sender(conn, num_words, filename, log):
     """ function to send messages to other end of pipe """
     '''
     open the file
@@ -30,47 +36,77 @@ def sender():
     Note: you must break each line in the file into words and
           send those words through the pipe
     '''
-    pass
+    log.write('sender awake')
+    with open(filename, 'rb') as f:
+        done = False
+        while not done:
+            block = f.read(BLOCK_SIZE)
+            if block:
+                conn.send(block)
+                num_words.value += 1
+                log.write('word sent')
+            else:
+                done = True
+    conn.send(END_MESSAGE)
+    conn.close()
+    log.write('sender done')
 
 
-def receiver():
+def receiver(conn, filename, log):
     """ function to print the messages received from other end of pipe """
     ''' 
     open the file for writing
     receive all content through the shared pipe and write to the file
     Keep track of the number of items sent over the pipe
     '''
-    pass
+    log.write('receiver awake')
+    with open(filename, 'wb') as f:
+        while True:
+            next_word = conn.recv()
+            if next_word == END_MESSAGE:
+                break
+            f.write(next_word)
+            log.write('word received')
+    conn.close()
+    log.write('receiver done')
 
 
 def are_files_same(filename1, filename2):
     """ Return True if two files are the same """
-    return filecmp.cmp(filename1, filename2, shallow = False) 
+    return filecmp.cmp(filename1, filename2, shallow=False)
 
 
-def copy_file(log, filename1, filename2):
-    # TODO create a pipe 
+def copy_file(log, source_filename, target_filename):
+    # TODO create a pipe
+    parent_conn, child_conn = mp.Pipe()
     
     # TODO create variable to count items sent over the pipe
+    num_words = mp.Value('L', 0)
 
-    # TODO create processes 
+    # TODO create processes
+    send_proc = mp.Process(target=sender, args=(parent_conn, num_words, source_filename, log))
+    receive_proc = mp.Process(target=receiver, args=(child_conn, target_filename, log))
 
     log.start_timer()
     start_time = log.get_time()
 
-    # TODO start processes 
-    
+    # TODO start processes
+    send_proc.start()
+    receive_proc.start()
+
     # TODO wait for processes to finish
+    send_proc.join()
+    receive_proc.join()
 
     stop_time = log.get_time()
 
-    log.stop_timer(f'Total time to transfer content = {PUT YOUR VARIABLE HERE}: ')
-    log.write(f'items / second = {PUT YOUR VARIABLE HERE / (stop_time - start_time)}')
+    log.stop_timer(f'Total time to transfer content = {num_words.value}: ')
+    log.write(f'items / second = {num_words.value / (stop_time - start_time)}')
 
-    if are_files_same(filename1, filename2):
-        log.write(f'{filename1} - Files are the same')
+    if are_files_same(source_filename, target_filename):
+        log.write(f'{source_filename} - Files are the same')
     else:
-        log.write(f'{filename1} - Files are different')
+        log.write(f'{source_filename} - Files are different')
 
 
 if __name__ == "__main__": 
@@ -80,5 +116,5 @@ if __name__ == "__main__":
     copy_file(log, 'gettysburg.txt', 'gettysburg-copy.txt')
     
     # After you get the gettysburg.txt file working, uncomment this statement
-    # copy_file(log, 'bom.txt', 'bom-copy.txt')
+    copy_file(log, 'bom.txt', 'bom-copy.txt')
 
