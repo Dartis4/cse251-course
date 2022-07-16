5b616a589395aaffdde3d94ecbaa7b962fde3cc8
Dane Artis <daneartis4@gmail.com>
2022-07-15 23:21:24 -0600

    In progress on final prove. Part 1 done. Part 2 works but not fast enough.

--- week14/assignment/functions.py
+++ week14/assignment/functions.py
@@ -37,17 +37,18 @@ Describe how to speed up part 2
 <Add your comments here>
 
 """
+import threading
 from queue import Queue
+from typing import Union
 
 from common import *
 
 TOP_API_URL = 'http://127.0.0.1:8123'
-NUM_GENERATIONS = 6
-NUM_FAMILIES = 2 ** NUM_GENERATIONS - 1
 
 
 # A few helper functions
 def retrieve_person(person_id):
+    # This function is mostly for testing
     thread = RequestThread(f'{TOP_API_URL}/person/{person_id}')
     thread.start()
     thread.join()
@@ -65,45 +66,55 @@ def retrieve_family(fam_id):
 def depth_fs_pedigree(family_id, tree):
     # implement Depth first retrieval
 
-    # Check for repeats and stay within generational restraint
-    if not tree.does_family_exist(family_id) and tree.get_family_count() < 3:
-        # Get the family info
-        family = Family(family_id, retrieve_family(family_id))
+    def search_generation(start_id):
+        # Check for repeats and stay within generational restraint
+        if not tree.does_family_exist(start_id):
+            # Get the family info
+            family = Family(start_id, retrieve_family(start_id))
+            # add family to tree
+            tree.add_family(family)
 
-        # Generate the threads for all family members and start them running
-        dad_thread = RequestThread(f'{TOP_API_URL}/person/{family.husband}')
-        dad_thread.start()
-        mom_thread = RequestThread(f'{TOP_API_URL}/person/{family.wife}')
-        mom_thread.start()
-        child_threads = [RequestThread(f'{TOP_API_URL}/person/{ident}') for ident in family.children]
-        list(map(lambda t: t.start(), child_threads))
+            d = family.husband
+            m = family.wife
 
-        # add family to tree
-        tree.add_family(family)
+            dad_thread = RequestThread(f'{TOP_API_URL}/person/{d}')
+            dad_thread.start()
+            mom_thread = RequestThread(f'{TOP_API_URL}/person/{m}')
+            mom_thread.start()
+            child_threads = [RequestThread(f'{TOP_API_URL}/person/{ident}') for ident in family.children]
+            list(map(lambda th: th.start(), child_threads))
 
-        # Make sure dad thread is done
-        dad_thread.join()
-        # Get info
-        dad = Person(dad_thread.response)
-        # Add them if not in tree
-        if not tree.does_person_exist(dad.id):
-            tree.add_person(dad)
-        # If there are parents
-        if dad.parents:
-            depth_fs_pedigree(dad.parents, tree)
+            for c_threads in child_threads:
+                c_threads.join()
+                child = Person(c_threads.response)
+                if not tree.does_person_exist(child.id):
+                    tree.add_person(child)
 
-        mom_thread.join()
-        mom = Person(mom_thread.response)
-        if not tree.does_person_exist(mom.id):
-            tree.add_person(mom)
-        if mom.parents:
-            depth_fs_pedigree(mom.parents, tree)
+            threads = []
 
-        for c_threads in child_threads:
-            c_threads.join()
-            child = Person(c_threads.response)
-            if not tree.does_person_exist(child.id):
-                tree.add_person(child)
+            if d and not tree.does_person_exist(d):
+                dad_thread.join()
+                dad = Person(dad_thread.response)
+                dad_parent = dad.parents
+                tree.add_person(dad)
+                if dad_parent:
+                    t = threading.Thread(target=search_generation, args=(dad_parent,))
+                    t.start()
+                    threads.append(t)
+
+            # Add them if not in tree
+            if m and not tree.does_person_exist(m):
+                mom_thread.join()
+                mom = Person(mom_thread.response)
+                mom_parent = mom.parents
+                tree.add_person(mom)
+                if mom_parent:
+                    t = threading.Thread(target=search_generation, args=(mom_parent,))
+                    t.start()
+                    threads.append(t)
+            list(map(lambda th: th.join(), threads))
+
+    search_generation(family_id)
 
 
 # -----------------------------------------------------------------------------
@@ -112,43 +123,43 @@ def breadth_fs_pedigree(start_id, tree):
     queue = Queue()
     queue.put(start_id)
 
-    while queue:
+    while not queue.empty():
         f = queue.get()
-        print(f)
-        family = Family(f, retrieve_family(f))
-        print(family)
-        if family.husband:
-            print((Person(retrieve_person(family.husband))).parents)
-        # if not tree.does_family_exist(f) and tree.get_family_count() < 15:
-        #     family = Family(f, retrieve_family(f))
-        #     tree.add_family(family)
-        #
-        #     dad_thread = RequestThread(f'{TOP_API_URL}/person/{family.husband}')
-        #     dad_thread.start()
-        #     mom_thread = RequestThread(f'{TOP_API_URL}/person/{family.wife}')
-        #     mom_thread.start()
-        #     child_threads = [RequestThread(f'{TOP_API_URL}/person/{ident}') for ident in family.children]
-        #     list(map(lambda t: t.start(), child_threads))
-        #
-        #     for c_threads in child_threads:
-        #         c_threads.join()
-        #         child = Person(c_threads.response)
-        #         if not tree.does_person_exist(child.id):
-        #             tree.add_person(child)
-        #
-        #     dad_thread.join()
-        #     dad = Person(dad_thread.response)
-        #     if not tree.does_person_exist(dad.id):
-        #         tree.add_person(dad)
-        #         if dad.parents:
-        #             queue.put(dad.parents)
-        #
-        #     mom_thread.join()
-        #     mom = Person(mom_thread.response)
-        #     if not tree.does_person_exist(mom.id):
-        #         tree.add_person(mom)
-        #         if mom.parents:
-        #             queue.put(mom.parents)
+        if f and not tree.does_family_exist(f):
+            family = Family(f, retrieve_family(f))
+            tree.add_family(family)
+
+            d = family.husband
+            m = family.wife
+
+            dad_thread = RequestThread(f'{TOP_API_URL}/person/{d}')
+            dad_thread.start()
+            mom_thread = RequestThread(f'{TOP_API_URL}/person/{m}')
+            mom_thread.start()
+            child_threads = [RequestThread(f'{TOP_API_URL}/person/{ident}') for ident in family.children]
+            list(map(lambda th: th.start(), child_threads))
+
+            for c_threads in child_threads:
+                c_threads.join()
+                child = Person(c_threads.response)
+                if not tree.does_person_exist(child.id):
+                    tree.add_person(child)
+
+            if d and not tree.does_person_exist(d):
+                dad_thread.join()
+                dad = Person(dad_thread.response)
+                dad_parent = dad.parents
+                tree.add_person(dad)
+                if dad_parent:
+                    queue.put(dad_parent)
+
+            if m and not tree.does_person_exist(m):
+                mom_thread.join()
+                mom = Person(mom_thread.response)
+                mom_parent = mom.parents
+                tree.add_person(mom)
+                if mom_parent:
+                    queue.put(mom_parent)
 
 
 # -----------------------------------------------------------------------------
